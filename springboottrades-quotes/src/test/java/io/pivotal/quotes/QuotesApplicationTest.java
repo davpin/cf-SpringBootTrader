7b9243d6d6adfc2f901350c452639fa3ffe48647
dpinto-pivotal <dpinto@pivotal.io>
2015-05-26 17:22:03 +0100

    Added test of quote application context

---
+++ springboottrades-quotes/src/test/java/io/pivotal/quotes/QuotesApplicationTest.java
@@ -0,0 +1,14 @@
+package io.pivotal.quotes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = QuotesApplication.class)
+public class QuotesApplicationTest {
+	@Test
+	public void contextLoads() {
+	}
+}
