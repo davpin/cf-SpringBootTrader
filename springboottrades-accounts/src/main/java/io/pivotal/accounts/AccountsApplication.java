23806278330b85f441065210aa358d8046840796
dpinto-pivotal <dpinto@pivotal.io>
2015-05-30 21:23:58 +0100

    all services to use eureka

--- springboottrades-accounts/src/main/java/io/pivotal/accounts/AccountsApplication.java
+++ springboottrades-accounts/src/main/java/io/pivotal/accounts/AccountsApplication.java
@@ -2,8 +2,10 @@ package io.pivotal.accounts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AccountsApplication {
 
 	public static void main(String[] args) {
--- springboottrades-web/src/main/java/io/pivotal/web/service/UserService.java
+++ springboottrades-web/src/main/java/io/pivotal/web/service/UserService.java
@@ -8,8 +8,7 @@ import io.pivotal.web.domain.AuthenticationRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.cloud.util.UriInfo;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -18,28 +17,27 @@ import org.springframework.web.client.RestTemplate;
 public class UserService {
 	private static final Logger logger = LoggerFactory
 			.getLogger(UserService.class);
-
-	@Autowired @Qualifier("accountService") UriInfo accountService;
 	
 	@Autowired
+	@LoadBalanced
 	private RestTemplate restTemplate;
 	
 	public void createAccount(Account account) {
 		logger.debug("Saving account with userId: " + account.getUserid());
-		String status = restTemplate.postForObject(accountService.getUri().toString()+"/account/", account, String.class);
+		String status = restTemplate.postForObject("http://accounts/account/", account, String.class);
 		logger.info("Status from registering account for "+ account.getUserid()+ " is " + status);
 	}
 	
 	public Map<String,Object> login(AuthenticationRequest request){
 		logger.debug("logging in with userId:" + request.getUsername());
-		Map<String,Object> result = (Map<String, Object>) restTemplate.postForObject(accountService.getUri()+"/login/".toString(), request, Map.class);
+		Map<String,Object> result = (Map<String, Object>) restTemplate.postForObject("http://accounts/login/".toString(), request, Map.class);
 		return result;
 	}
 	
 	public Account getAccount(String user) {
 		logger.debug("Looking for account with userId: " + user);
 		
-	    Account account = restTemplate.getForObject(accountService.getUri().toString()+"/account/?name={user}", Account.class, user);
+	    Account account = restTemplate.getForObject("http://accounts/account/?name={user}", Account.class, user);
 	    logger.debug("Got Account: " + account);
 	    return account;
 	}
@@ -47,7 +45,7 @@ public class UserService {
 	public void logout(String user) {
 		logger.debug("logging out account with userId: " + user);
 		
-	    ResponseEntity<?> response = restTemplate.getForEntity(accountService.getUri().toString()+"/logout/{user}", String.class, user);
+	    ResponseEntity<?> response = restTemplate.getForEntity("http://accounts/logout/{user}", String.class, user);
 	    logger.debug("Logout response: " + response.getStatusCode());
 	}
 	
