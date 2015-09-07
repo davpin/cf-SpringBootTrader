2ce47974ec8e936d7dc7b5deee70921b645595a2
dpinto-pivotal <dpinto@pivotal.io>
2015-09-07 13:19:01 +0100

    Adding FilterRegistrationBean - traceFilter - as it seems to be missing from autoconfig

--- springboottrades-web/src/main/java/io/pivotal/web/WebApplication.java
+++ springboottrades-web/src/main/java/io/pivotal/web/WebApplication.java
@@ -1,13 +1,22 @@
 package io.pivotal.web;
 
 
+import java.util.regex.Pattern;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.sleuth.Sampler;
+import org.springframework.cloud.sleuth.Trace;
+import org.springframework.cloud.sleuth.instrument.scheduling.TraceSchedulingAspect;
+import org.springframework.cloud.sleuth.instrument.web.TraceFilter;
 import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
+import org.springframework.util.StringUtils;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -17,6 +26,19 @@ public class WebApplication {
 	public Sampler<?> defaultSampler() {
 		return new AlwaysSampler();
 	}
+	
+	String skipPattern = "";
+	@Autowired
+	private Trace trace;
+	@Bean
+	public FilterRegistrationBean traceFilter(ApplicationEventPublisher publisher) {
+		Pattern pattern = StringUtils.hasText(this.skipPattern) ? Pattern.compile(this.skipPattern)
+				: TraceFilter.DEFAULT_SKIP_PATTERN;
+		TraceFilter filter = new TraceFilter(this.trace, pattern);
+		filter.setApplicationEventPublisher(publisher);
+		return new FilterRegistrationBean(filter);
+	}
+	
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
     }
